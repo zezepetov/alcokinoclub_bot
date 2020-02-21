4e3611b0d9d500ba99a2351ed25e13ee35732ca1
zezepetov <61234028+zezepetov@users.noreply.github.com>
2020-02-21 11:53:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,15 @@ from aiogram.utils.executor import start_webhook
 
 TOKEN = os.environ['TOKEN']
 
+WEBHOOK_HOST = 'https://deploy-heroku-bot.herokuapp.com'  # name your app
+WEBHOOK_PATH = '/webhook/'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.environ.get('PORT')
+
+logging.basicConfig(level=logging.INFO)
+
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
