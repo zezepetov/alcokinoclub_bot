44ee8637a7409100a1746a74b5201bca500a7e4f
zezepetov <61234028+zezepetov@users.noreply.github.com>
2020-02-21 12:45:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,44 +7,12 @@ from aiogram.utils.executor import start_webhook
 
 TOKEN = os.environ['TOKEN']
 
-WEBHOOK_HOST = 'https://alkokinoclubot.herokuapp.com'  # name your app
-WEBHOOK_PATH = '/webhook/'
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+bot = telebot.TeleBot(TOKEN)
 
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.environ.get('PORT')
-
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
-
-@dp.message_handler(commands='start')
-async def welcome(message: types.Message):
-    await bot.send_message(
-        message.chat.id,
-        f'Приветствую! Это демонтрационный бот\n'
-        f'Подробная информация на '
-        f'{md.hlink("github", "https://github.com/alcokinoclub_bot/heroku")}',
-        parse_mode=types.ParseMode.HTML,
-        disable_web_page_preview=True)
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await bot.send_message(message.chat.id, message.text)
-
-
-async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
-
-
-async def on_shutdown(dp):
-    # insert code here to run it before shutdown
-    pass
-
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
-    start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
-                  on_startup=on_startup, on_shutdown=on_shutdown,
-host=WEBAPP_HOST, port=WEBAPP_PORT)
+     bot.polling(none_stop=True)
+	 
