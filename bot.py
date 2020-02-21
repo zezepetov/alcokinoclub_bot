6f3980ed9e9f8ff24c673ddd8aa9bedc8afffe08
zezepetov <61234028+zezepetov@users.noreply.github.com>
2020-02-21 11:58:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,10 +18,35 @@ WEBAPP_PORT = os.environ.get('PORT')
 
 logging.basicConfig(level=logging.INFO)
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands='start')
+async def welcome(message: types.Message):
+    await bot.send_message(
+        message.chat.id,
+        f'Приветствую! Это демонтрационный бот\n'
+        f'Подробная информация на '
+        f'{md.hlink("github", "https://github.com/deploy-your-bot-everywhere/heroku")}',
+        parse_mode=types.ParseMode.HTML,
+        disable_web_page_preview=True)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await bot.send_message(message.chat.id, message.text)
+
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+
+
+async def on_shutdown(dp):
+    # insert code here to run it before shutdown
+    pass
+
 
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
-	 
+    start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
+                  on_startup=on_startup, on_shutdown=on_shutdown,
+host=WEBAPP_HOST, port=WEBAPP_PORT)
